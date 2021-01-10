daa2094a8659194e0e570162733bd7c3a0fff834
Larko <59736843+Larkooo@users.noreply.github.com>
2021-01-10 00:24:08 -0500

    Bonjour, blabla shows the name of the user now

--- lib/pages/homeSkeleton.dart
+++ lib/pages/homeSkeleton.dart
@@ -2,9 +2,11 @@ import 'package:alert/alert.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:myschool/models/user.dart';
+import 'package:myschool/services/database.dart';
 import 'home.dart';
 import 'calendar.dart';
 import 'announcements.dart';
+import 'package:provider/provider.dart';
 
 class HomeSkeleton extends StatefulWidget {
   final UserData user;
@@ -22,11 +24,13 @@ class _HomeState extends State<HomeSkeleton> {
     //showSlideDialog(context: context, child: Text("Testing welcome message"));
   }
 
+  static UserData userData;
+
   int _selectedIndex = 0;
   static List<Widget> _widgetOptions = <Widget>[
-    Home(),
-    Announcements(),
-    Calendar()
+    Home(user: userData),
+    Announcements(user: userData),
+    Calendar(user: userData)
   ];
 
   void _onItemTapped(int index) {
@@ -37,49 +41,60 @@ class _HomeState extends State<HomeSkeleton> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-              child: Text(
-                'Bonjour, meow.',
-                style: TextStyle(
-                  fontSize: 20.0,
+    final firebaseUser = context.watch<User>();
+    return StreamBuilder(
+        stream: DatabaseService(uid: firebaseUser.uid).user,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            userData = snapshot.data;
+            return Scaffold(
+              appBar: AppBar(),
+              drawer: Drawer(
+                child: ListView(
+                  children: <Widget>[
+                    DrawerHeader(
+                      child: Text(
+                        'Bonjour, ${userData.firstName}',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      leading: Text('Parametres'),
+                      trailing: Icon(Icons.settings),
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      leading: Text('Deconnexion'),
+                      trailing: Icon(Icons.logout),
+                      onTap: () {
+                        FirebaseAuth.instance.signOut();
+                        Alert(message: "Déconnecté").show();
+                      },
+                    ),
+                  ],
                 ),
               ),
-            ),
-            ListTile(
-              leading: Text('Parametres'),
-              trailing: Icon(Icons.settings),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Text('Deconnexion'),
-              trailing: Icon(Icons.logout),
-              onTap: () {
-                FirebaseAuth.instance.signOut();
-                Alert(message: "Déconnecté").show();
-              },
-            ),
-          ],
-        ),
-      ),
-      body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Accueil"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.announcement), label: "Annonces"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_today), label: "Calendrier")
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
-    );
+              body: _widgetOptions.elementAt(_selectedIndex),
+              bottomNavigationBar: BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.home), label: "Accueil"),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.announcement), label: "Annonces"),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.calendar_today), label: "Calendrier")
+                ],
+                currentIndex: _selectedIndex,
+                onTap: _onItemTapped,
+              ),
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        });
   }
 }
