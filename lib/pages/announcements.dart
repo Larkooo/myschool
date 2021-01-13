c4dea464ed7a47b70e4c5dea8b37169030947991
Larko <59736843+Larkooo@users.noreply.github.com>
2021-01-13 10:53:50 -0500

    changes to announcements page

--- lib/pages/announcements.dart
+++ lib/pages/announcements.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -43,28 +44,130 @@ class _AnnouncementsState extends State<Announcements> {
                                 mainAxisSize: MainAxisSize.min,
                                 children: [
                               ListTile(
-                                leading: RichText(
-                                    text: TextSpan(children: [
-                                  WidgetSpan(child: Icon(Icons.person)),
-                                  TextSpan(text: announcement['author'])
-                                ])),
-                                title: Text(announcement['title']),
-                                subtitle: Text(announcement['description']),
-                              ),
-                              Row(
-                                //mainAxisAlignment: MainAxisAlignment.end,
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  Text("Posté le " +
-                                      DateFormat.yMMMMEEEEd().format(
-                                          (announcement['createdAt']
-                                                  as Timestamp)
-                                              .toDate())),
-                                ],
-                              ),
+                                  //leading: RichText(
+                                  //    text: TextSpan(children: [
+                                  //  WidgetSpan(child: Icon(Icons.person)),
+                                  //  TextSpan(text: announcement['author'])
+                                  //])),
+                                  title: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            FutureBuilder(
+                                                future: (announcement['author']
+                                                        as DocumentReference)
+                                                    .get(),
+                                                builder: (context, snapshot) {
+                                                  if (snapshot.hasData) {
+                                                    Map<String, dynamic> data =
+                                                        snapshot.data.data();
+                                                    return Row(
+                                                      children: [
+                                                        ClipRRect(
+                                                            borderRadius:
+                                                                BorderRadius
+                                                                    .circular(
+                                                                        30),
+                                                            child: data['avatarUrl'] !=
+                                                                    null
+                                                                ? CachedNetworkImage(
+                                                                    imageUrl: data[
+                                                                        'avatarUrl'],
+                                                                    progressIndicatorBuilder: (context,
+                                                                            url,
+                                                                            downloadProgress) =>
+                                                                        CircularProgressIndicator(
+                                                                            value:
+                                                                                downloadProgress.progress),
+                                                                    errorWidget: (context,
+                                                                            url,
+                                                                            error) =>
+                                                                        Icon(Icons
+                                                                            .error),
+                                                                    height: 20,
+                                                                    width: 20,
+                                                                  )
+                                                                : Container(
+                                                                    width: 20,
+                                                                    height: 20,
+                                                                    color: Colors
+                                                                            .grey[
+                                                                        900],
+                                                                    child: Icon(
+                                                                      Icons
+                                                                          .person,
+                                                                      size: 10,
+                                                                    ))),
+                                                        SizedBox(
+                                                          width: 5,
+                                                        ),
+                                                        Text(data['firstName'])
+                                                      ],
+                                                    );
+                                                  } else {
+                                                    return CircularProgressIndicator();
+                                                  }
+                                                }),
+                                            SizedBox(
+                                              width: 5,
+                                            ),
+                                            Text(
+                                              DateFormat.yMMMMEEEEd().format(
+                                                  (announcement['createdAt']
+                                                          as Timestamp)
+                                                      .toDate()),
+                                              style: TextStyle(
+                                                  color: Colors.grey[500],
+                                                  fontSize: 13),
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Text(announcement['title']),
+                                            Spacer(),
+                                            Container(
+                                              width: 50,
+                                              height: 20,
+                                              decoration: BoxDecoration(
+                                                color: Colors.grey[700],
+                                                borderRadius:
+                                                    BorderRadius.circular(10),
+                                              ),
+                                              child: Material(
+                                                  borderRadius:
+                                                      BorderRadius.circular(10),
+                                                  child: InkWell(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              10),
+                                                      onTap: () => {},
+                                                      child: Center(
+                                                          child: Text(
+                                                        "École",
+                                                        style: TextStyle(
+                                                            fontSize: 10),
+                                                      )))),
+                                            )
+                                          ],
+                                        ),
+                                      ]),
+                                  subtitle: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text((announcement['description']
+                                                        as String)
+                                                    .length <
+                                                100
+                                            ? announcement['description']
+                                            : (announcement['description']
+                                                        as String)
+                                                    .substring(0, 100) +
+                                                "..."),
+                                      ])),
                               SizedBox(
                                 height: 5,
                               ),
