c3cd89ce8f8133cf056a8a32d1bf8e55bf9137af
Larko <59736843+Larkooo@users.noreply.github.com>
2021-01-14 15:25:44 -0500

    working on home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,6 +4,8 @@ import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:intl/intl.dart';
+import 'package:myschool/components/login.dart';
 import 'package:myschool/models/user.dart';
 import 'package:myschool/services/database.dart';
 import 'package:myschool/services/firebase_storage.dart';
@@ -21,7 +23,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List remoteSchoolDays;
   bool dayIsHome = false;
-  DateTime today = DateTime.now();
+  DateTime now = DateTime.now();
+  Map<String, dynamic> nextCourse;
 
   @override
   Widget build(BuildContext context) {
@@ -32,19 +35,31 @@ class _HomeState extends State<Home> {
           if (snapshot.hasData) {
             UserData userData = snapshot.data;
             return FutureBuilder(
-              future: StorageService(
-                      ref: "/schools/${userData.school.uid}/remoteschool.json")
-                  .getDownloadURL(),
+              future: Future.wait([
+                StorageService(
+                        ref:
+                            "/schools/${userData.school.uid}/remoteschool.json")
+                    .getDownloadURL(),
+                StorageService(
+                        ref:
+                            "/schools/${userData.school.uid}/groups/${userData.school.group.uid}/timetable.json")
+                    .getDownloadURL()
+              ]),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   return FutureBuilder(
-                    future: DefaultCacheManager().getSingleFile(snapshot.data),
+                    future: Future.wait([
+                      DefaultCacheManager().getSingleFile(snapshot.data[0]),
+                      DefaultCacheManager().getSingleFile(snapshot.data[1])
+                    ]),
                     builder: (context, snapshot) {
                       if (snapshot.hasData) {
-                        (jsonDecode((snapshot.data as File).readAsStringSync()))
+                        File remoteSchoolDays = snapshot.data[0];
+                        File schoolTimetable = snapshot.data[1];
+                        (jsonDecode(remoteSchoolDays.readAsStringSync()))
                             .forEach((element) {
                           DateTime remoteDay = DateTime.parse(element['date']);
-                          if (today.isSameDay(remoteDay)) {
+                          if (now.isSameDay(remoteDay)) {
                             if (userData.school.group.uid.startsWith("5")) {
                               dayIsHome = element["5"] == 1 ? true : false;
                             } else if (userData.school.group.uid
@@ -58,6 +73,16 @@ class _HomeState extends State<Home> {
                             }
                           }
                         });
+                        for (final e in (jsonDecode(
+                            schoolTimetable.readAsStringSync()))) {
+                          DateTime courseStart = DateTime.parse(
+                              e['dateDebut'] + "T" + e['heureDebut']);
+                          if (courseStart > now) {
+                            nextCourse = e;
+                            break;
+                          }
+                        }
+
                         return Column(
                           children: [
                             Container(
@@ -68,6 +93,33 @@ class _HomeState extends State<Home> {
                                   child: Text(
                                       dayIsHome ? "À la maison" : "À l'école")),
                             ),
+                            SizedBox(
+                              height: 50,
+                            ),
+                            Text(
+                              "Prochain cours",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Container(
+                                width: MediaQuery.of(context).size.width / 1.3,
+                                child: Card(
+                                  child: ListTile(
+                                      title: Text(nextCourse['description'] +
+                                          " (${nextCourse['locaux'][0]})"),
+                                      subtitle: Text(nextCourse['intervenants']
+                                              [0]['nom'] +
+                                          " " +
+                                          nextCourse['intervenants'][0]
+                                              ['prenom'] +
+                                          " - Dans " +
+                                          (DateTime.parse(nextCourse[
+                                                          'dateDebut'] +
+                                                      "T" +
+                                                      nextCourse['heureDebut'])
+                                                  .differenceInHours(now))
+                                              .toString() +
+                                          " heures")),
+                                )),
                           ],
                         );
                       } else {
