015491981755fd5d6c7e7b86f3af65a5dc6eae7a
Larko <59736843+Larkooo@users.noreply.github.com>
2021-04-10 20:40:54 -0400

    Revert "updated version to 0.0.2"
    This reverts commit 81b5c2991dec91ef03af0cf4972daf005b67adf0.

--- lib/components/mozaik_login.dart
+++ lib/components/mozaik_login.dart
@@ -45,7 +45,6 @@ class _MozaikLoginState extends State<MozaikLogin> {
           onLoadStart: (controller, url) async {
             String urlString = url.toString();
             if (urlString.contains('#id_token')) {
-              // trick to recognize url query parameters
               url = Uri.parse(urlString.replaceFirst('#', '?'));
 
               Mozaik.idToken = url.queryParameters['id_token'];
@@ -53,7 +52,7 @@ class _MozaikLoginState extends State<MozaikLogin> {
 
               SharedPreferences prefs = await SharedPreferences.getInstance();
               prefs.setString('mozaikUserData', jsonEncode(Mozaik.payload));
-              // user logged in at least one time, used to know if user has to type his password and email when needed to login
+              // user logged in at least one time
               prefs.setBool('mozaikLoyal', true);
 
               Navigator.pop(context);
